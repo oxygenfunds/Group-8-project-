a65b674091e14bf9249c2bd492fdd74cfbad891b
UGWOKE SAMUEL EKENEDILICHUKWU <juniorkc61@gmail.com>
2026-05-25 00:46:49 +0200

    fix bugs and update code

--- index.php
+++ index.php
@@ -2,12 +2,54 @@
 $name = $email = $reg_number = $course = $password ="" ;
     
 
-    $conn = new mysqli("localhost", "root", "", "group8_db");
+    $conn = new mysqli("localhost", "root", "", "group_8db");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
     
+
+$message = "";
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+
+$username = $_POST['username'];
+$email = $_POST['email'];
+$reg_number = $_POST['reg_number'];
+$course = $_POST['course'];
+$password = $_POST['password'];
+
+if (empty($username))
+    echo "Name is required";
+elseif (empty($email))
+    echo "Email is required";
+elseif (empty($reg_number))
+    echo "Registration number is required";
+elseif (empty($course))
+    echo "Course is required";
+elseif (empty($password))
+    echo "Password is required";
+
+else{
+    $hash_password = password_hash($password, PASSWORD_DEFAULT);
+
+    $sql = "INSERT INTO students (username, email, reg_number, course, password) 
+            VALUES ('$username', '$email', '$reg_number', '$course', '$hash_password')";
+
+        if(mysqli_query($conn, $sql)){
+            echo "<p style='color:green;'>✅ Data inserted successfully</p>";
+        } else {
+            echo "<p style='color:red;'>❌ Error: " . mysqli_error($conn) . "</p>";
+        }
+}
+$message = "Data inserted successfully";
+
+}
+
+
+
+
+
  ?>
 
 <!DOCTYPE html>
@@ -23,10 +65,11 @@ if ($conn->connect_error) {
 </head>
 
 <body>
+    <?php echo "hello world"; ?>
     
-
     <div class="container">
         <div class="heading">
+
             <h1>GROUP 8 FORM SUBMISSION</h1>
             <p> welcome to Group 8 Form Submission Pratical, 
                 please fill the form below to submit your details</p>  
@@ -67,7 +110,7 @@ if ($conn->connect_error) {
 </body>
 </html>
 
-<?php 
+<!-- <
 
 if($_SERVER["REQUEST_METHOD"] == "POST") {
 
@@ -104,9 +147,9 @@ else{
 }
 
 mysqli_close($conn);
-?>
 
 
+ -->
 
 
 
